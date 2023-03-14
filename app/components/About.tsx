f3783289897171d9d8215898de9df1b7cd5edc48
Thom Allen <thomwork@gmail.com>
2023-03-14 21:05:39 +0000

    Add some quote unquote personality

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -12,6 +12,33 @@ export function About() {
               Yo! I&apos;m Thom, a front-end dev based in London with 10+ years
               experience. Most recently lead FE at Zzish.
             </p>
+            <p>Probably thinking too much about:</p>
+            <ul>
+              <li>
+                <a
+                  href="https://www.youtube.com/watch?v=X82QTT_vJI0"
+                  title="Zeta Gundam OP1"
+                >
+                  1979&ndash;1988 period Mobile Suit Gundam
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://en.wikipedia.org/wiki/Long-term_nuclear_waste_warning_messages"
+                  title="Long-term nuclear waste warning messages on Wikipedia. Treat yourself"
+                >
+                  Long-term nuclear waste warning messages
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.youtube.com/watch?v=CUmmxW7Ksc8"
+                  title="The Pricemaster. Everything is for sale"
+                >
+                  The Pricemaster
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
         <div className="mb-4 flex basis-1/2 p-2">
